d772d9332f8f98f09e180a820ec30da913cb0124
Jambuuul <122870717+Jambuuul@users.noreply.github.com>
2024-04-04 16:13:06 +0300

    Create Floyd.cpp

---
+++ graphs/Floyd.cpp
@@ -0,0 +1,9 @@
+//this algo is so easy there is actually no point in this, but im still add it
+
+for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                d[j][k] = min(d[j][k], d[j][i] + d[i][k]);
+            }
+        }
+}
